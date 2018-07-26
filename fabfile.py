710c9cb4295f70a55d9b0fc0c501ab1e2730de55
staneyffer <staneyffer@gmail.com>
2018-07-26 17:00:00 +0800

    deploy config

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd, env, sudo
 from fabric.contrib.console import confirm
 import os
+import asyncio
 
 if os.path.exists('.env'):
     print('Importing environment from .env...')
@@ -23,20 +24,33 @@ env.password = os.environ.get('ENVPASSWORD')
 
 server_dir = os.environ.get('ENV_SERVER_DIR')
 local_dir = os.environ.get('ENV_LOCAL_FILE')
-
-
-def deploy():
-    build()
-    upload()
-    with cd(server_dir):
-        sudo('systemctl restart nginx')
+app_name = os.environ.get('ENV_APP_NAME')
 
 
 def upload():
-    file_name = 'dist'
+    file_name = '.nuxt'
     upload_script = 'scp -r ' + local_dir + file_name + ' ' + env.user + '@' + env.hosts[0] + ':' + server_dir
     local(upload_script)
 
-
-def build():
+async def build():
     local('npm run build')
+    upload()
+
+async def push_and_pull():
+    local('git push')
+    with cd(server_dir):
+        run('git pull')
+        run('cnpm install')
+
+def pm2():
+    with cd(server_dir):
+        text = 'pm2 restart ' + app_name
+        run(text)
+        sudo('systemctl restart nginx')
+
+def deploy():
+    loop = asyncio.get_event_loop()
+    tasks = [build(), push_and_pull()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+    pm2()
