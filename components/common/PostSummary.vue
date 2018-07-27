f8f92127be54b305520e4ecbd0d08e2a23c0847d
staneyffer <staneyffer@gmail.com>
2018-07-27 22:38:49 +0800

    调整页面样式

--- components/common/PostSummary.vue
+++ components/common/PostSummary.vue
@@ -8,16 +8,10 @@
           height="200px"
           :src="post.img">
       </v-card-media>
-      <v-container fill-height fluid>
-        <v-layout fill-height>
-          <v-flex xs12 align-end flexbox>
-            <v-card-title>
-              <span class="headline mx-auto">{{ post.title }}</span>
-            </v-card-title>
-          </v-flex>
-        </v-layout>
-      </v-container>
 
+      <v-card-title>
+        <span class="headline mx-auto">{{ post.title }}</span>
+      </v-card-title>
       <v-card-title v-show="true">
         <span>{{post.summary}}...</span>
       </v-card-title>
@@ -25,32 +19,32 @@
       <v-divider></v-divider>
 
       <v-card-actions>
-        <v-card-title :to="`/category/` + post.category.id">
+        <span @click.prevent="toInfo(`/category/` + post.category.id)">
           <v-icon>note</v-icon>
           <span>{{post.category.name}}</span>
-        </v-card-title>
+        </span>
         <div
             v-bind:class="{'hidden-sm-and-down': true}"
             v-for="tag in post.tags" :key="tag.id">
-          <v-card-title :to="`/tag/` + tag.id" >
+          <div @click.prevent="toInfo(`/tag/` + tag.id)">
             <v-icon>bookmark</v-icon>
             <span>{{tag.name}}</span>
-          </v-card-title>
+          </div>
         </div>
         <div
             v-bind:class="{'hidden-sm-and-down': true}"
             v-for="(sec, index) in post.sections" :key="index">
-          <v-card-title :to="`/section/` + sec.id">
+          <div @click.prevent="toInfo(`/section/` + sec.id)">
             <v-icon>view_column</v-icon>
             <span>{{sec.name}}</span>
-          </v-card-title>
+          </div>
         </div>
         <v-spacer></v-spacer>
         <v-icon>date_range</v-icon>
         <span>{{post.createTime}}</span>
-        <v-card-title :to="`/post/` + post.id">
+        <div @click.prevent="toInfo(`/post/` + post.id)">
           <v-btn flat color="blue" >更多</v-btn>
-        </v-card-title>
+        </div>
       </v-card-actions>
 
     </v-card>
@@ -72,6 +66,13 @@
         tagUrl: '/tag/',
         cateUrl: '/category/',
         sectionUrl: '/section/'
+      }
+    },
+    methods: {
+      toInfo (routerPath) {
+        this.$router.push({
+          path: routerPath
+        })
       }
     }
   }
